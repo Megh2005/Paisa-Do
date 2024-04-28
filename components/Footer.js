ddaac1b0dd418204154d6eaaed589594cdf23588
Megh Deb <20169629-pheonixflaunt@users.noreply.gitlab.com>
2024-04-28 21:08:29 +0530

    upload

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer>
-        <p>Paisa Do Bhai</p>
-    </footer>
-  )
+    return (
+        <footer className='bg-[#111115] text-white px-4 h-16 flex items-center justify-center'>
+            <p className='text-center'>Copyright &copy; Fund Me Here 💰 - All Rights Reserved</p>
+        </footer>
+    )
 }
 
 export default Footer
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <nav>
-        Navbar
-    </nav>
-  )
+    return (
+        <nav className='bg-gray-900 text-white flex justify-between px-4 h-16 items-center'>
+            <div className='logo font-bold text-lg'>💰 Fund Me Here</div>
+            <ul className='flex justify-between gap-4'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Sign Up</li>
+                <li>Log In</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
